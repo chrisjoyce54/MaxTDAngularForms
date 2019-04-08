c4b58dfd4c5852531c8a88cac44b41ecd11b15c7
Chris Joyce <chris@joycenet.us>
2019-04-08 14:05:35 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    name: '',
+    mail: '',
+    question: '',
+    ans: '',
+    gen: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userdata: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'female'
+    // });
+    this.signupForm.form.patchValue({
+      userdata: {
+        username: suggestedName
+      }
+    });
+  }
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.name = this.signupForm.value.userdata.username;
+    this.user.mail = this.signupForm.value.userdata.email;
+    this.user.question = this.signupForm.value.secret;
+    this.user.ans = this.signupForm.value.questionAnswer;
+    this.user.gen = this.signupForm.value.gender;
   }
 }
